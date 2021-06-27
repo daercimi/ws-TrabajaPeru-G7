558b0e09ec378f1c3fc180f3025129d8b0d39cd8
ff <dd>
2021-06-26 22:30:27 -0500

    Actualizacion

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ api.all("*", function(req, res, next) {
 });
 
 api.get("/", function(req, res) {
-    res.send("Web service Trabaja Perú (:");
+    res.send("Web service Trabaja Perú");
 });
 
 api.post("/user", user);
