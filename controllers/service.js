c10f2e713abea298c47b4a97dffb7b38cfbfd485
daercimi <daercimi@gmail.com>
2021-07-05 11:02:17 -0500

    core: Completado controlador de Servicios, falta front para probar

--- controllers/service.js
+++ controllers/service.js
@@ -2,13 +2,29 @@ const Servicio = require("../models/Servicios");
 const dbConnection = require("../connect");
 const connect = require("../connect");
 const connection = dbConnection();
+const middleware = require("../middleware/auth");
 
-serviceOperation = function(req,middleware,res) {
+function validateUser(middleware){
+    /* La idea en esta función es que me devuelva el
+    us_id si el usuario está logeado, de lo contrario
+    que me devuelva null, luego ese us_id se usará para 
+    todo lo demás */
+}
+
+
+serviceOperation = function(req,us_id,res) {
 
     const command = req.body.command;
     switch(command){
         case "CREATE_SERVICE":
-            validationCreateService(req,middleware,res);
+            if(us_id == null){
+                return res.status(200).send({
+                    status: "ERROR",
+                    message: "Servicios.js/serviceOperation() - CREATE_SERVICE: Usuario no loggeado",                    
+                })
+            } else {
+                validationCreateService(req,us_id,res);
+            }
             break;
 
         case "SEARCH_SERVICE":
@@ -16,29 +32,42 @@ serviceOperation = function(req,middleware,res) {
             break;
 
         case "EDIT_SERVICE":
-            //editService(req,middleware,res);
-            //break;
-            return res.status(200).send({
-                status: "ERROR",
-                message: "OPERACION TODAVÍA NO IMPLEMENTADA EN EL CONTROLADOR :´v",
-            })
+            if(us_id == null){
+                return res.status(200).send({
+                    status: "ERROR",
+                    message: "Servicios.js/serviceOperation() - EDIT_SERVICE: Usuario no loggeado",                    
+                })
+            } else {
+                editService(req,us_id,res);
+            }
+            break;
 
         case "DELETE_SERVICE":
-            //deleteService(req,middleware,res);
-            //break;
-            return res.status(200).send({
-                status: "ERROR",
-                message: "OPERACION TODAVÍA NO IMPLEMENTADA EN EL CONTROLADOR :´v",
-            })
+            if(us_id == null){
+                return res.status(200).send({
+                    status: "ERROR",
+                    message: "Servicios.js/serviceOperation() - DELETE_SERVICE: Usuario no loggeado",                    
+                })
+            } else {
+                deleteService(req,us_id,res);
+            }
+            break;
 
-        case "GET_SERVICES":
+        case "GET_HOME_SERVICES":
             //Los primeros 10 para el home
-            //getServices(res);
-            //break;
-            return res.status(200).send({
-                status: "ERROR",
-                message: "OPERACION TODAVÍA NO IMPLEMENTADA EN EL CONTROLADOR :´v",
-            })
+            getHomeServices(res);
+            break;
+
+        case "GET_MY_SERVICES":
+            if(us_id == null){
+                return res.status(200).send({
+                    status: "ERROR",
+                    message: "Servicios.js/serviceOperation() - GET_MY_SERVICES: Usuario no loggeado",                    
+                })
+            } else {
+                getMyServices(us_id,res);
+            }
+            break;
 
         default:
             return res.status(500).send({
@@ -48,7 +77,7 @@ serviceOperation = function(req,middleware,res) {
     }
 }
 
-function validationCreateService(req,middleware,res) {
+function validationCreateService(req,us_id,res) {
     /*Se creará el servicio solo si no existe una combinación (us_id, cat_id) 
     Además si es que ya ha existido antes y esa combinación, se colocará la nueva
     descripción e imagen, pero el resto de datos se mantendrá. Esto es para evitar
@@ -56,9 +85,9 @@ function validationCreateService(req,middleware,res) {
     sin bajas calificaciones.
     */
 
-    const user = middleware.us_id // REVISAR
+    const user = us_id 
     const service = req.body.transaction;
-    const newServicio = Servicio(user, service); // REVISAR
+    const newServicio = Servicio(user, service);
 
     servicioExiste = serviceExists(newServicio.us_id,cat_id);
 
@@ -89,7 +118,7 @@ function validationCreateService(req,middleware,res) {
 
 function serviceExists(us_id,cat_id){
 
-    sql_existe = "SELECT * FROM Servicios WHERE us_id = ? AND cat_id = ?", [us_id,cat_id];
+    sql_existe = "SELECT * FROM Servicios WHERE us_id = ? AND cat_id = ? ;", [us_id,cat_id];
 
     connection.connect();
     connection.query(sql_existe, (err, result) =>{
@@ -120,7 +149,7 @@ function serviceExists(us_id,cat_id){
 
 function recoverService(newServicio,res){
     sql_recuperar = "UPDATE Servicio SET " +
-    "ser_descripcion = ?, ser_imagen = ?, ser_eliminado = 0", [newServicio.ser_descripcion, newServicio.ser_imagen];
+    "ser_descripcion = ?, ser_imagen = ?, ser_eliminado = 0 ;", [newServicio.ser_descripcion, newServicio.ser_imagen];
 
     connection.connect();
     connection.query(sql_recuperar, (err, result) =>{
@@ -143,7 +172,7 @@ function recoverService(newServicio,res){
 function createService(newServicio,res){
     sql_insertar = "INSERT INTO Servicio " +
     "(us_id, cat_id, ser_descripcion, ser_imagen)" 
-    + " VALUES (?,?,?,?)", [newServicio.us_id, newServicio.cat_id, newServicio.ser_descripcion, newServicio.ser_imagen];
+    + " VALUES (?,?,?,?) ;", [newServicio.us_id, newServicio.cat_id, newServicio.ser_descripcion, newServicio.ser_imagen];
 
     connection.connect();
     connection.query(sql_insertar, (err, result) =>{
@@ -172,7 +201,7 @@ function searchCategory(req, res) {
     2) Buscar los servicios que tienen ese cat_id*/
     const CatName = req.body.transaction;
     connection.connect()
-    sql_buscarCat = "SELECT * FROM Categoria WHERE cat_nombre LIKE %?%", [CatName];
+    sql_buscarCat = "SELECT * FROM Categoria WHERE cat_nombre LIKE %?% ;", [CatName];
 
     connection.query(sql_buscarCat, (err, result) => {
         if (err) {
@@ -195,7 +224,7 @@ function searchCategory(req, res) {
 
 function searchService(cat_id){
     connection.connect()
-    sql_buscarSer = "SELECT * FROM Servicio WHERE cat_id = ?", [cat_id];
+    sql_buscarSer = "SELECT * FROM Servicio WHERE cat_id = ? ;", [cat_id];
 
     connection.query(sql_buscarSer, (err, result) => {
         if (err) {
@@ -207,6 +236,104 @@ function searchService(cat_id){
             return result;
         }
     });
+}
+
+
+/* ///////////////////////////////////////////////////////////////////// */
+
+
+function editService(req,us_id,res){
+
+    const user = us_id
+    const service = req.body.transaction;
+
+    sql_editar = "UPDATE SERVICIO SET " +
+    "ser_descripcion = ?, ser_imagen = ? WHERE us_id = ? AND cat_id = ? ;", [service.ser_descripcion, service.ser_imagen,us_id,service.cat_id];
+
+    connection.connect();
+    connection.query(sql_editar, (err, result) =>{
+
+        if (err) {
+            return res.status(200).send({
+                status: "FAILED",
+                message: err,
+            });
+        } else {
+            return res.status(200).send({
+                status: "SUCCESS",
+                message: "Servicio editado correctamente",
+            });
+        }
+    });
+    
+}
+
+
+/* ///////////////////////////////////////////////////////////////////// */
+
+
+function deleteService(req,us_id,res){
+
+    cat_id = req.body.transaction.cat_id; //REVISAR
+    sql_eliminar = "UPDATE SERVICIO SET ser_eliminado = 1 WHERE us_id = ? AND cat_id = ?",[us_id,cat_id]
+
+    connection.connect();
+    connection.query(sql_eliminar,(err, result) =>{
+        if (err) {
+            return res.status(200).send({
+                status: "FAILED",
+                message: err,
+            });
+        } else {
+            return res.status(200).send({
+                status: "SUCCESS",
+                message: "Servicio eliminado correctamente",
+            });
+        }        
+    });
+}
+
+
+/* ///////////////////////////////////////////////////////////////////// */
+
+
+function getHomeServices(res){
+
+    sql_getHomeServices = "SELECT * FROM Services LIMIT 10;"
+
+    connection.connect();
+    connection.query(sql_getHomeServices,(err, result) =>{
+        if (err) {
+            return res.status(200).send({
+                status: "FAILED",
+                message: err,
+            });
+        } else {
+            return res.status(200).send(JSON.stringify(result));
+        }        
+    });
+}
+
+
+/* ///////////////////////////////////////////////////////////////////// */
+
+
+function getMyServices(us_id,res){
+
+    sql_getMyServices = "SELECT * FROM Services WHERE us_id = ? ;",[us_id]
+
+    connection.connect();
+    connection.query(sql_getMyServices, (err,result) => {
+        if (err) {
+            return res.status(200).send({
+                status: "FAILED",
+                message: err,
+            });
+        } else {
+            return res.status(200).send(JSON.stringify(result));
+        }         
+    });
+
 }
 
 
