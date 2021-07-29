de6c5a07a7c9449972c854cc04390a95f3043b39
daercimi <daercimi@gmail.com>
2021-07-28 19:34:16 -0500

    fixed: token

--- services/index.js
+++ services/index.js
@@ -4,7 +4,7 @@ var config = require('../GlobalEnv');
 
 function createToken(user){
 	const payload = {
-		"sub" : user.id,
+		"sub" : user.us_id,
 		"iat" : moment().unix(),
 		"exp": moment().add(30, "days").unix(),
 	};
